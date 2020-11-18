c80d3a4b7d47fc05e69e1131a14030404f8da1e1
unknown <jad.bennouna@outlook.fr>
2020-11-18 11:28:30 -0500

    Start from social media toolbar 156

--- app/src/main/java/com/example/tatthood/MapsActivity.java
+++ app/src/main/java/com/example/tatthood/MapsActivity.java
@@ -1,14 +1,14 @@
 package com.example.tatthood;
 
-import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.FragmentActivity;
-
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
@@ -19,8 +19,9 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
+
+//import com.google.android.libraries.places.api.model.Place;
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -54,7 +55,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * installed Google Play services and returned to the app.
      */
 
-
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
@@ -82,22 +82,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
     // able to be know if the permission was given by the user
     private void showUserCurrentLocation(){
+ 
+
+
         //context is this because the method is called from the MapActivity
         if(ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
             accessLocationPermission();
         } else {
-            fusedLocationClient.getLastLocation().addOnCompleteListener(new OnCompleteListener<Location>() {
-                @Override
-                public void onComplete(@NonNull Task<Location> task) {
-                   Location location = task.getResult();
-                    if(location != null) {
-                        LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-                        mMap.addMarker(new MarkerOptions().position(latLng).title("Your current location is here"));
-                        CameraUpdate cameraLocation = CameraUpdateFactory.newLatLngZoom(latLng,16.0f);
-                        mMap.moveCamera(cameraLocation);
-                    } else {
-                       Toast.makeText(MapsActivity.this, "Something went wrong. Please try again",Toast.LENGTH_SHORT).show();
-                    }
+
+            fusedLocationClient.getLastLocation().addOnCompleteListener(task -> {
+               Location location = task.getResult();
+                if(location != null) {
+                    LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                    mMap.addMarker(new MarkerOptions().position(latLng).title("Your current location is here"));
+                    CameraUpdate cameraLocation = CameraUpdateFactory.newLatLngZoom(latLng,16.0f);
+                    mMap.moveCamera(cameraLocation);
+
+
+
+                } else {
+                   Toast.makeText(MapsActivity.this, "Something went wrong. Please try again",Toast.LENGTH_SHORT).show();
                 }
             });
 
